8950e078abdc2b8b4ea35621edf8ffd03e4782cd
grudzug <grudzug@gmail.com>
2022-01-24 16:13:37 +0100

    useReducer, useContext added

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,17 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './CounterSlice'
+
+export function Counter() {
+  const count = useSelector(state => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <span>{count}</span>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </div>
+  )
+}
